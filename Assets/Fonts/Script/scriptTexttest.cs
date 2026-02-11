ab7637484afaf6b87bb05fafca2ab215ab72d940
taylnoa580 <taylnoa580@gmail.com>
2026-02-11 13:15:37 -0600

    Upgrade/radio panels scale properly
    Upgrade/radio panels have been integrated into the new HUD. Fixed the opening cutscene resolution/textbox scaling. updated some packages we were using. unity ai will work one day

--- Assets/Fonts/Script/scriptTexttest.cs
+++ Assets/Fonts/Script/scriptTexttest.cs
@@ -1,19 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using TMPro;
 
-public class scriptTexttest : MonoBehaviour 
+namespace JazzCreateGames.BubbleFont.Free
 {
-	public Text bubbleText;
-    public Text bubbleTitleTxt;
-    public GameObject bubble;
-    public GameObject bubbleTitle;
-	
-	void Start ()
-	{
-        bubbleTitleTxt.text = "Font JazzCreate Bubble";
-		bubbleText.text = "☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼▬↨↑↓→←\n" + "∟↔▲▼!#$%&'()*+,-./0123456789:;<=>?@\n"
-            + "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n`abcdefghijklmnopqrstuvwxyz\nîìÄÅÉæÆôöòûùÿÖÜ \n ¢£¥ƒáíóúñÑªº¿¬½¼¡«»░▒▓\n "
-                + "│┤╣║╗┐└┴┬├─┼╚╔╩╦╣║╗•§©®¶" + "\nThanks for using fonts from JazzCreates2015©.";
-	}
+    public class ScriptTextTest : MonoBehaviour
+    {
+        public TMP_Text bubbleText;
+        public TMP_Text bubbleTitleTxt;
+        public GameObject bubble;
+        public GameObject bubbleTitle;
+
+        void Start()
+        {
+            bubbleTitleTxt.text = "Font JazzCreate Bubble";
+
+            bubbleText.text =
+                "☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼▬↨↑↓→←\n" +
+                "∟↔▲▼!#$%&'()*+,-./0123456789:;<=>?@\n" +
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n" +
+                "`abcdefghijklmnopqrstuvwxyz\n" +
+                "îìÄÅÉæÆôöòûùÿÖÜ \n" +
+                "¢£¥ƒáíóúñÑªº¿¬½¼¡«»░▒▓\n" +
+                "│┤╣║╗┐└┴┬├─┼╚╔╩╦╣║╗•§©®¶_\n" +
+                "Thanks for using fonts from JazzCreatesGames 2015–2025 ©.";
+        }
+    }
 }
